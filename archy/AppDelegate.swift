5dfdda46654038bda5f507f791f3b76265cbab2a
Rizky Adipratama <adipratama.rizky@gmail.com>
2019-08-22 13:52:13 +0700

    -Create UIView Video Preview -Video Player

--- archy/AppDelegate.swift
+++ archy/AppDelegate.swift
@@ -16,6 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+       
+        
+        let initialViewController = PreviewVC (nibName: "PreviewVC", bundle: nil)
+            
+        self.window?.rootViewController = initialViewController
+        self.window?.makeKeyAndVisible()
+        
+        
         return true
     }
 
--- archy/Library/Preview/PreviewVC.swift
+++ archy/Library/Preview/PreviewVC.swift
@@ -1,30 +1,94 @@
-//
-//  PreviewVC.swift
-//  archy
-//
-//  Created by Eibiel Sardjanto on 22/08/19.
-//  Copyright © 2019 Eibiel Sardjanto. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
+import AVKit
+
 
 class PreviewVC: UIViewController {
 
+    var playerController = AVPlayerViewController()
+    var player: AVPlayer?
+ 
+    
+    @IBAction func playVideoButton(_ sender: Any) {
+        playVideo()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        alertInputScore()
+        urlVideo()
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func urlVideo() {
+        let videoString:String? = Bundle.main.path(forResource:"videoarcher2", ofType: ".MOV")
+        
+        if let url = videoString {
+            
+            let videoURL = NSURL(fileURLWithPath: url)
+            
+            self.player = AVPlayer(url: videoURL as URL)
+            self.playerController.player = self.player
+        }
     }
-    */
+    
+    
+    func playVideo()
+    {
+        self.present(self.playerController, animated: true, completion: {
+            
+            self.playerController.player?.play()
+            
+        })
+    }
 
+    func alertInputScore()
+    {
+      
+        let alert = UIAlertController(title: "Input Your Total Score", message: "Your score will be seen in Preview", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Input your total score"
+        }
+        
+       
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak alert] (_) in
+            let textField = alert?.textFields![0]
+            print("Text field: \(textField!.text)")
+        }))
+
+        
+        let alertWindow = UIWindow(frame: UIScreen.main.bounds)
+        alertWindow.rootViewController = UIViewController()
+        alertWindow.windowLevel = UIWindow.Level.alert + 1
+        
+        alertWindow.makeKeyAndVisible()
+        
+        alertWindow.rootViewController?.present(alert, animated:true, completion: nil)
+    }
+    
+    
+//    func generateThumbnailVideo() {
+//        let videoString:String? = Bundle.main.path(forResource:"videoarcher", ofType: ".mp4")
+//
+//        if let url = videoString {
+//            let fileUrl = URL(fileURLWithPath: url)
+//            var avAsset = AVURLAsset(url: fileUrl, options: nil)
+//            var imageGenerator = AVAssetImageGenerator(asset: avAsset)
+//            imageGenerator.appliesPreferredTrackTransform = true
+//            var thumbnail: UIImage?
+//
+//            do {
+//                thumbnail = try UIImage(cgImage: imageGenerator.copyCGImage(at: CMTime(seconds: 0, preferredTimescale: 1), actualTime: nil))
+//
+//
+//              self.videoThumbnail.image = thumbnail!
+//            } catch let e as NSError {
+//                print("Error: \(e.localizedDescription)")
+//            }
+//        }
+//    }
+    
 }
